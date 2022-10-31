7ea11388d80716b0b4df6416799f44f3f10232a8
juliano <juliano@MacBook-Pro-de-juliano.local>
2022-10-31 07:40:42 -0300

    008

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -419,6 +419,27 @@ $(document).ready(function () {
     });
     //Fim do script para o componente
     //Inicio do script para o componente
+    var emLiteratura = $('.css_06_boxe_em_literatura');
+    emLiteratura.on('click', function () {
+      console.log('02');
+      var randomId = Math.floor(Math.random() * 100000) + 1;
+
+      if ($(this).find('input').attr('id').length == "0") {
+        console.log('ID alterado');
+        $(this).find('input').attr('id', randomId);
+      } else {
+        console.log('Ja contem ID');
+      }
+
+      if ($(this).find('label').attr('for').length == "0") {
+        console.log('FOR alterado');
+        $(this).find('label').attr('for', randomId);
+      } else {
+        console.log('Ja contem FOR');
+      }
+    });
+    //Fim do script para o componente
+    //Inicio do script para o componente
     var emMatematica = $('.css_06_boxe_em_matematica');
     emMatematica.on('click', function () {
       console.log('02');
